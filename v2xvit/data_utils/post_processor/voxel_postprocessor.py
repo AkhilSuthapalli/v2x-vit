9a3ca86545bbee274cd71fe44de5041a535ead08
Akhil Suthapalli <akhilsuthapalli@gmail.com>
2026-08-07 14:27:06 +0100

    Update4: defaulting to matplotlib
    using defaulting to matplotlib from open3d to fix the error

--- v2xvit/data_utils/post_processor/voxel_postprocessor.py
+++ v2xvit/data_utils/post_processor/voxel_postprocessor.py
@@ -415,8 +415,22 @@ class VoxelPostprocessor(BasePostprocessor):
             opencood dataset object.
 
         """
-        vis_utils.visualize_single_sample_output_gt(pred_box_tensor,
-                                                    gt_tensor,
-                                                    pcd,
-                                                    show_vis,
-                                                    save_path)
+        if dataset is not None:
+            # Uses OpenCOOD's built-in 2D BEV projector
+            vis_utils.visualize_single_sample_output_bev(
+                pred_box_tensor,
+                gt_tensor,
+                pcd,
+                dataset,
+                show_vis=show_vis,
+                save_path=save_path
+            )
+        else:
+            # Fallback to 3D output
+            vis_utils.visualize_single_sample_output_gt(
+                pred_box_tensor,
+                gt_tensor,
+                pcd,
+                show_vis,
+                save_path
+            )
